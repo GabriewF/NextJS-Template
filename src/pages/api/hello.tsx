f80c633554e0906cb0a71d1804e8fa7dfa1802bc
Felipe Gabriel <85371036+gabriewf@users.noreply.github.com>
2022-04-28 09:50:36 -0300

    Update hello.tsx

--- src/pages/api/hello.tsx
+++ src/pages/api/hello.tsx
@@ -2,21 +2,34 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 // Api Handler
-const HelloHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+async function HelloHandler(req: NextApiRequest, res: NextApiResponse) {
   ///
   try {
     switch (req.method) {
       case 'GET': {
         // Response function
-        const Response: any = (): boolean => {
-          // Response
-          res.status(200).send({
+        async function Response(): Promise<boolean> {
+          // Response Body Interface
+          interface ResponseBody {
+            statusCode: number,
+            statusText: string,
+            success: boolean,
+            method: string | undefined,
+            message?: string,
+            error?: unknown
+          }
+
+          // Response Body
+          const responseBody: ResponseBody = {
             statusCode: 200,
             statusText: 'OK',
             success: true,
             method: req.method,
             message: `Hello World!`
-          })
+          }
+
+          // Response
+          res.status(200).send(responseBody)
 
           // Break the request
           return true
@@ -29,30 +42,71 @@ const HelloHandler = async (req: NextApiRequest, res: NextApiResponse) => {
         break
       }
       default: {
-        // Response
-        res.status(405).send({
-          statusCode: 405,
-          statusText: `Method Not Allowed`,
-          success: false,
-          method: req.method,
-        })
+        // Response function
+        async function Response(): Promise<boolean> {
+          // Response Body Interface
+          interface ResponseBody {
+            statusCode: number,
+            statusText: string,
+            success: boolean,
+            method: string | undefined,
+            message?: string,
+            error?: unknown
+          }
 
-        // Break the request
-        return false
+          // Response Body
+          const responseBody: ResponseBody = {
+            statusCode: 405,
+            statusText: `Method Not Allowed`,
+            success: false,
+            method: req.method,
+          }
+
+          // Response
+          res.status(405).send(responseBody)
+
+          // Break the request
+          return false
+        }
+
+        //Calling functions
+        await Response()
+
+        // End
+        break
       }
     }
   } /* Catch error */ catch (err: unknown) {
-    // Response
-    res.status(500).send({
-      statusCode: 500,
-      statusText: 'Internal Server Error',
-      success: false,
-      method: req.method,
-      error: err,
-    })
+    // Response function
+    async function Response(): Promise<boolean> {
+      // Response Body Interface
+      interface ResponseBody {
+        statusCode: number,
+        statusText: string,
+        success: boolean,
+        method: string | undefined,
+        message?: string,
+        error?: unknown
+      }
 
-    // Break the request
-    return false
+      // Response Body
+      const responseBody: ResponseBody = {
+        statusCode: 500,
+        statusText: 'Internal Server Error',
+        success: false,
+        method: req.method,
+        error: err,
+      }
+
+      // Response
+      res.status(500).send(responseBody)
+
+      // Break the request
+      return false
+    }
+
+    //Calling functions
+    await Response()
   }
 }
 
